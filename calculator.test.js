892176a3c8e2533bc300b67edf7f560fc0bdfe3f
Ramon <ramon.pereira@prof.unibh.br>
2021-10-27 21:38:38 -0300

    Created calculator test

---
+++ calculator.test.js
@@ -0,0 +1,14 @@
+
+const calculator = require('./calculator');
+
+test("testando soma de 1+1"), () => {
+    expect(calculator.soma(1,1)).toBe(2);
+}
+
+test("testando multiplicacao de 2*2"), () => {
+    expect(calculator.multiplicacao(2,2)).toBe(4);
+}
+
+test("testando divisao de 10/2"), () => {
+    expect(calculator.divisao(10,2)).toBe(4);
+}
